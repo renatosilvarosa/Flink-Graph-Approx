9753e9cf41cd58531d61b8bd6c550029749704f0
Renato Rosa <renato.rosa@tecnico.ulisboa.pt>
2016-08-02 00:33:27 +0100

    Approximating HITS

---
+++ Tester/src/main/java/test/hits/ApproximatedHITSStatistics.java
@@ -0,0 +1,53 @@
+package test.hits;
+
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.operators.Order;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.graph.Graph;
+import org.apache.flink.graph.library.link_analysis.HITS;
+import org.apache.flink.types.NullValue;
+import pt.tecnico.graph.algorithm.hits.ApproximatedHITS;
+import pt.tecnico.graph.stream.GraphUpdateTracker;
+
+/**
+ * Created by Renato on 26/07/2016.
+ */
+public class ApproximatedHITSStatistics extends HITSStatistics {
+
+    public ApproximatedHITSStatistics(String dir, String fileName) {
+        super(fileName, dir);
+    }
+
+    @Override
+    public void onStart() throws Exception {
+        super.onStart();
+        printStream.println("iteration;nVertices;nEdges;time");
+    }
+
+    @Override
+    public ApproximatedHITS.DeciderResponse onQuery(int id, String query, Graph<Long, NullValue, NullValue> graph, GraphUpdateTracker<Long, NullValue, NullValue> updateTracker) {
+        return ApproximatedHITS.DeciderResponse.COMPUTE_APPROXIMATE;
+    }
+
+    @Override
+    public void onQueryResult(int id, String query, ApproximatedHITS.DeciderResponse response, Graph<Long, NullValue, NullValue> graph,
+                              DataSet<Long> computedVertices, DataSet<HITS.Result<Long>> result, JobExecutionResult jobExecutionResult) {
+        try {
+            long nVertices = graph.numberOfVertices();
+            long nEdges = graph.numberOfEdges();
+
+            printStream.format("%d;%d;%d;%d;%d%n", id, nVertices, nEdges, computedVertices.count(), jobExecutionResult.getNetRuntime());
+            printStream.flush();
+
+            result.sortPartition("f1.f0", Order.DESCENDING).project(0).writeAsCsv(String.format("%s/approx-result-hub-%02d.csv", dir, id),
+                    System.lineSeparator(), ";", FileSystem.WriteMode.OVERWRITE);
+
+            result.sortPartition("f1.f1", Order.DESCENDING).project(0).writeAsCsv(String.format("%s/approx-result-auth-%02d.csv", dir, id),
+                    System.lineSeparator(), ";", FileSystem.WriteMode.OVERWRITE);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
